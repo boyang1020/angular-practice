ca7e90ab2afff14420e246c61d12b83f34288e97
boyang1020 <boyang1020@gmail.com>
2019-07-08 19:52:36 -0700

    finished

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  name = '';
+
+  onClick() {
+    if (!name) {
+      this.name = '';
+    }
+  }
 }
